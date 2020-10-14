1a903dd18df4f520eeea083be768166094fa498d
Ethan-NDHU <71751245+Ethan-NDHU@users.noreply.github.com>
2020-10-14 14:34:48 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,11 +7,11 @@ basic.forever(function () {
     if (degrees < 45) {
         basic.showString("N")
     } else if (degrees < 135) {
-        basic.showString("East")
+        basic.showString("E")
     } else if (degrees < 225) {
-        basic.showString("South")
+        basic.showString("S")
     } else if (degrees < 315) {
-        basic.showString("West")
+        basic.showString("W")
     } else {
         basic.showString("N")
     }
